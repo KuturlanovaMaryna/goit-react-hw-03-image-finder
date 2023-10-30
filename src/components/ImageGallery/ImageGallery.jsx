637e2e75ceb6ea91bdf49cd008ff31e11a646e15
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-10-30 19:29:30 +0200

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,11 +2,6 @@ import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
 const ImageGallery = ({ images, handleImage }) => {
-  // const handleImage = event => {
-  //   const currentImg = event.target.attributes[3].value;
-  //   console.dir(currentImg);
-  //   openModal(currentImg);
-  // };
   return (
     <ul className={css.imageContainer} onClick={handleImage}>
       {images !== null &&
